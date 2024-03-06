1f6785fb83c58ed531f97afa27e5b37898cbfa33
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-05 16:58:49 -0800

    Refactor Header nav buttons to use single function prop

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,39 +8,25 @@ export default function Header(props) {
 
   return (
     <div className="navBar">
-        <div>
-          <img className="profilePic" src={profilePic} alt="silhouette of Aaron sitting on beach watching sunset" />
-        </div>
-        <img className="name" src={nameWordArt} alt="Aaron Mejia name" />
-        <br className="wideViewOnly" /><br className="wideViewOnly" />
-        <button type="button" className="expandSection" id="firstNavSection" onClick={props.onClickHome}>Home</button>
-        {/* <div className="aboutMe-Section">
-          <p>Hello!<br />I am an up-and-coming developer in the Portland metro area. I come from a background of 5 years in IT, managing the maintenance of large networks, training of technicians, and creation/documentation of processes. Programming excites me and I <em>love</em> helping individuals further themselves and their businesses through technology. I am currently enrolled in <span ><a href="https://epicodus.com" target="_blank" rel="noreferrer">Epicodus</a></span> to help achieve this goal.<br />I'm excited to work with you!</p>
-        </div> */}
-        <br className="wideViewOnly" />
-        <button type="button" className="expandSection" id="secondNavSection" onClick={props.onClickAbout}>About</button>
-        {/* <div className="aboutMe-Section">
-          <ul>
-            <li>I have assisted in the creation & design of at least 4 different websites for small businesses.</li>
-            <li>I have built fully-functioning web servers from scratch on Linux using LAMP stacks and WordPress</li>
-          </ul>
-        </div> */}
-        <br className="wideViewOnly" />
-        <button type="button" className="expandSection" id="thirdNavSection" onClick={props.OnClickContact}>Contact</button>
-        {/* <div className="aboutMe-Section">
-          <p>Let's get in touch!</p>
-          <p>Email: <a href="mailto:aaron@example.com">aaron@example.com</a>
-            <br />
-            GitHub: <a href="https://github.com/mejia-dev">github.com/mejia-dev</a>
-          </p>
-        </div> */}
-        <br className="wideViewOnly" />
+      <img className="name" src={nameWordArt} alt="Aaron Mejia name" />
+      <div>
+        <img className="profilePic" src={profilePic} alt="silhouette of Aaron sitting on beach watching sunset" />
       </div>
+      
+      <br className="wideViewOnly" />
+      <button type="button" className="expandSection" onClick={() => props.onClickNavButton("Home")}>Home</button>
+
+      <br className="wideViewOnly" />
+      <button type="button" className="expandSection" onClick={() => props.onClickNavButton("About")}>About</button>
+
+      <br className="wideViewOnly" />
+      <button type="button" className="expandSection" onClick={() => props.onClickNavButton("Contact")}>Contact</button>
+
+      <br className="wideViewOnly" />
+    </div>
   )
 }
 
 Header.propTypes = {
-  onClickHome: PropTypes.func,
-  onClickAbout: PropTypes.func,
-  OnClickContact: PropTypes.func
+  onClickNavButton: PropTypes.func
 }
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -31,29 +31,31 @@ export default function Portfolio() {
       });
   }, [])
 
-
-  function showPageAbout(){
-    setSelectedPage("About");
-  }
-
-  function showPageContact(){
-    setSelectedPage("Contact");
-  }
-
-  function showPageProjects()
-  {
-    setSelectedPage("Home");
-  }
-
   let currentView;
   if (selectedPage === "Contact"){
     currentView = (
       <>
+      {/* <div className="aboutMe-Section">
+          <p>Let's get in touch!</p>
+          <p>Email: <a href="mailto:aaron@example.com">aaron@example.com</a>
+            <br />
+            GitHub: <a href="https://github.com/mejia-dev">github.com/mejia-dev</a>
+          </p>
+        </div> */}
       </>
     )
   } else if (selectedPage === "About"){
     currentView = (
       <>
+      {/* <div className="aboutMe-Section">
+          <p>Hello!<br />I am an up-and-coming developer in the Portland metro area. I come from a background of 5 years in IT, managing the maintenance of large networks, training of technicians, and creation/documentation of processes. Programming excites me and I <em>love</em> helping individuals further themselves and their businesses through technology. I am currently enrolled in <span ><a href="https://epicodus.com" target="_blank" rel="noreferrer">Epicodus</a></span> to help achieve this goal.<br />I'm excited to work with you!</p>
+        </div> */}
+        {/* <div className="aboutMe-Section">
+          <ul>
+            <li>I have assisted in the creation & design of at least 4 different websites for small businesses.</li>
+            <li>I have built fully-functioning web servers from scratch on Linux using LAMP stacks and WordPress</li>
+          </ul>
+        </div> */}
       </>
     )
   } else {
@@ -79,9 +81,7 @@ export default function Portfolio() {
   return (
     <React.Fragment>
       <Header 
-        onClickHome={showPageProjects}
-        onClickAbout={showPageAbout}
-        OnClickContact={showPageContact}
+        onClickNavButton={setSelectedPage}
       />
       {currentView}
     </React.Fragment>

c55964e0325b08f7b54964aa5249ad0819f5cfaa
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-09 19:21:42 -0800

    Add contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Contact() {
+
+  // const [projectsListLoaded, setProjectsListLoaded] = useState(false);
+  // const [projectsList, setProjectsList] = useState([]);
+  // const [projectsListApiErrorMsg, setProjectsListApiErrorMsg] = useState(null);
+
+  function SubmitForm(event) {
+    event.preventDefault();
+    const formElement = document.getElementById("contactForm");
+    const submittedData = new FormData(formElement);
+    fetch(`${process.env.REACT_APP_CONTACT_FORM_API}`,
+      {
+        method: "POST",
+        body: submittedData
+      }
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status}`);
+        }
+        console.log(response);
+        return response;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   return (
     <React.Fragment>
-        <h3>Contact Form</h3>
-        <p>Contact contact contact! I love contact</p>
-        <p>Email: <a href="mailto:aaron@example.com">aaron@example.com</a>
-            <br />
-            GitHub: <a href="https://github.com/mejia-dev">github.com/mejia-dev</a>
-          </p>
+      <h3>Contact Form</h3>
+      <p>Contact contact contact! I love contact</p>
+      <form id="contactForm" onSubmit={(event) => SubmitForm(event)}>
+        <input placeholder="Your Name" name="Name" type="text" required />
+        <input placeholder="Your Email" name="Email" type="email" required />
+        <input placeholder="Your Message" name="Message" type="text" required />
+        <input type="submit" />
+      </form>
     </React.Fragment>
   )
 }

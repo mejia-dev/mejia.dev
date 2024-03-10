41634b658c9034cb8b8a4a893db064723ebce85d
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-09 20:03:49 -0800

    Change form contents to inclue autocompletes and use textarea instead of input

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -34,7 +34,7 @@ export default function Contact() {
   if (formSubmitSuccess) {
     statusMessage = <span>Message sent! I'll be in touch with you soon.</span>
   } else if (formSubmitLoading) {
-    statusMessage = <span>Sending message . . .</span>
+    statusMessage = <span>Sending message...</span>
   } else if (formSubmitError != null) {
     statusMessage = <span>Error: {formSubmitError}</span>
   }
@@ -44,9 +44,9 @@ export default function Contact() {
       <h3>Contact Form</h3>
       <p>Contact contact contact! I love contact</p>
       <form id="contactForm" onSubmit={(event) => SubmitForm(event)}>
-        <input placeholder="Your Name" name="Name" type="text" required />
-        <input placeholder="Your Email" name="Email" type="email" required />
-        <input placeholder="Your Message" name="Message" type="text" required />
+        <input placeholder="Your Name" name="Name" type="text" autoComplete="name" required />
+        <input placeholder="Your Email" name="Email" type="email" autoComplete="email" required />
+        <textarea name="Message" rows="5" cols="30" placeholder="Your message here..." required/>
         <input type="submit" />
       </form>
       <p>{statusMessage}</p>

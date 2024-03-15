3046c370262fb3e4bdaea19e661050e02f488ff9
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-15 16:44:21 -0700

    Update photo class names

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,8 +36,8 @@ export default function Header(props) {
         <svg onClick={toggleModalDisplay} xmlns="http://www.w3.org/2000/svg" fill="currentColor" id="navBarHamburger" viewBox="0 0 16 16">
           <path d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
         </svg>
-        <img id="nameWordArt" src={nameWordArt} alt="Aaron Mejia name" />
-        <img id="profilePic" src={profilePic} alt="Silhouette of Aaron sitting on beach watching sunset" />
+        <img id="navBarPhotoNameWordArt" src={nameWordArt} alt="Aaron Mejia name" />
+        <img id="navBarPhotoProfilePic" src={profilePic} alt="Silhouette of Aaron sitting on beach watching sunset" />
         <Modal
           isOpen={modalDisplay}
           onRequestClose={toggleModalDisplay}

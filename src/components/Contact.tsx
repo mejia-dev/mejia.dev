dcd1886d63c1d00b4b3c8e1fe8cba891165b4fee
mejia-dev <aaron.mejia8484@gmail.com>
2024-05-18 18:56:07 -0700

    Improve contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -47,14 +47,21 @@ export default function Contact(): JSX.Element {
 
   return (
     <>
-      <h3>Contact Form</h3>
-      {/* <p>Contact contact contact! I love contact</p> */}
+      <h3>Contact</h3>
+      <p>Whether you have an idea for a project or just want to chat, feel free to send me a message!</p>
       <form id="contactForm" onSubmit={(event) => submitForm(event)}>
-        <input placeholder="Your Name" name="Name" type="text" autoComplete="name" required />
-        <input placeholder="Your Email" name="Email" type="email" autoComplete="email" required />
+        Name<br /> 
+        <input placeholder="Your Name" name="Name" type="text" autoComplete="name" required /><span className="requiredField"> *</span><br /><br />
+        Email Address<br />
+        <input placeholder="Your Email" name="Email" type="email" autoComplete="email" required /><span className="requiredField"> *</span><br /><br />
         {/* rows="5" cols="30" */}
-        <textarea name="Message"  placeholder="Your message here..." required />
-        <input type="submit" />
+        Your Message<br />
+        <textarea name="Message"  placeholder="Your message here..." required /><span className="requiredField"> *</span><br /><br /><br />
+        <button type="submit">
+          Send <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-send-fill" viewBox="0 0 16 16">
+  <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z"/>
+</svg>
+        </button>
       </form>
       <p>{statusMessage}</p>
     </>

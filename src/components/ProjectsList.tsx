c3f4ecb147b46c84db880e497806f9701fc7d0b0
mejia-dev <aaron.mejia8484@gmail.com>
2024-05-24 11:13:21 -0700

    Add radio buttons to control project list viewer type

--- src/components/ProjectsList.tsx
+++ src/components/ProjectsList.tsx
@@ -1,7 +1,22 @@
+import { useState } from "react"
+
 export default function ProjectsList(): JSX.Element {
+
+  const [viewerType, setViewerType] = useState<string>("List");
+
   return (
     <>
       <h3>Projects List</h3>
+      <div id="projectsListControlsDiv">
+      <form>
+          <input type="radio" id="projectsListViewByList" name="projectsListViewBy" onChange={() => setViewerType("List")} value="List"/>
+          <input type="radio" id="projectsListViewByGrid" name="projectsListViewBy" onChange={() => setViewerType("Grid")} value="Grid"/>
+          <input type="radio" id="projectsListViewByCarousel" name="projectsListViewBy" onChange={() => setViewerType("Carousel")} value="Carousel"/>
+        </form>
+      </div>
+      <div id="projectsListContent" className={`projectsViewer${viewerType}`}>
+        {viewerType}
+      </div>
     </>
   )
 }

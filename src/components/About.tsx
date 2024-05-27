ae0a75b667860dbdfdb857d6bb648e3f67eb9039
mejia-dev <aaron.mejia8484@gmail.com>
2024-05-26 19:05:47 -0700

    Update title of About page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,11 @@
 export default function About(): JSX.Element {
   return (
     <>
-      <h3>About</h3>
+      <h2>Hey, I'm Aaron</h2>
+      <p>What do you need? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex aspernatur distinctio explicabo, repellendus voluptatum iusto doloribus necessitatibus tempore est rem. Impedit mollitia in repellendus consequatur, officiis autem totam iste nesciunt.
+        <br />
+        I can help with that
+      </p>
     </>
   )
 }

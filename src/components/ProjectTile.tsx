f19495874e14e70ee27c1018c999892969024ce4
mejia-dev <aaron.mejia8484@gmail.com>
2024-05-31 18:19:22 -0700

    Refactor project details display to be separate function

--- src/components/ProjectTile.tsx
+++ src/components/ProjectTile.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+// import ReactModal, { Styles } from "react-modal";
 import noImage from "../assets/noImg.png";
 
 interface ProjectTileProps {
@@ -15,8 +16,34 @@ interface ImageObject {
 }
 
 export default function ProjectTile(props: ProjectTileProps): JSX.Element {
-  // this should call GH API to check stars
   const [imgSrc, setImgSrc] = useState<string>("");
+  const [modalShown, setModalShown] = useState<boolean>(false);
+
+  function handleOnClickAction(): void {
+    // if displayType is carousel or list, do nothing
+    // if displayType is grid, show Modal
+
+    // alternatively, useEffect to check if displayType is list, and append OnClick action to show modal if needed?
+  }
+
+  function renderProjectDetails(): JSX.Element {
+    return (
+      <div>
+        <p>{props.desc}</p>
+        <p>
+          <a href={"https://github.com/mejia-dev/" + props.repoName} target="_blank">GitHub Repository</a>
+          {props.liveLink != "" && (
+            <>
+              <br />
+              <a href={props.liveLink} target="_blank">Live Site</a>
+            </>
+          )}
+        {/* add languages/techs here? */}
+        {/* add stars here? */}
+        </p>
+      </div>
+    )
+  }
 
   useEffect(() => {
     async function getImgSrc(): Promise<void> {
@@ -31,27 +58,26 @@ export default function ProjectTile(props: ProjectTileProps): JSX.Element {
     getImgSrc();
   }, [props.photoName])
 
+  // useEffect(() => {
+  //   ReactModal.setAppElement(document.getElementById("mainBodyContent") as HTMLElement)
+  // }, [])
+
   return (
     <div className={"projectTile" + props.displayType}>
       <img src={imgSrc} alt={props.title + " screenshot"} />
       <div>
         <span>{props.title}</span>
-        {props.displayType === "List" && (
-          <div>
-            <p>{props.desc}</p>
-            <p>
-              <a href={"https://github.com/mejia-dev/" + props.repoName} target="_blank">GitHub Repository</a>
-              {props.liveLink != "" && (
-                <>
-                <br />
-                <a href={props.liveLink} target="_blank">Live Site</a>
-                </>
-              )}
-              
-            </p>
-          </div>
-
-        )}
+        {/* <ReactModal
+          contentLabel={props.title + " Project Details"}
+          isOpen={modalShown}
+          onRequestClose={() => setModalShown(!modalShown)}
+        // style={modalStyling}
+        >
+          {renderProjectDetails()}
+        </ReactModal> */}
+        {props.displayType === "List" &&
+          renderProjectDetails()
+        }
       </div>
     </div>
   )

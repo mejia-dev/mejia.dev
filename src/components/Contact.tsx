07a3e5720d192b9dfcbeb62b6d131c9848fa7c1e
mejia-dev <aaron.mejia8484@gmail.com>
2024-06-02 21:07:00 -0700

    Update placement of elements on page

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -48,22 +48,22 @@ export default function Contact(): JSX.Element {
   return (
     <>
       <h2>Contact</h2>
-      <p>Whether you have an idea for a project or just want to chat, feel free to send me a message!</p>
+      <span>Whether you have an idea for a project or just want to chat, feel free to send me a message!</span>
       <form id="contactForm" onSubmit={(event) => submitForm(event)}>
         <div id="contactFormDetails">
           <input placeholder="Name" name="Name" type="text" autoComplete="name" required />
           <input placeholder="Email" name="Email" type="email" autoComplete="email" required />
         </div>
-        
         <textarea name="Message" cols={30} rows={10} placeholder="Your message here..." required />
-        <br />
         <button type="submit">
           Send <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-send-fill" viewBox="0 0 16 16">
             <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z" />
           </svg>
         </button>
+        <br />
+        {statusMessage}
       </form>
-      <p>{statusMessage}</p>
+      
     </>
   )
 }

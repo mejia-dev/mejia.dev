b9dc01a7d5d3bc287e64faea197016e7c999b99c
mejia-dev <aaron.mejia8484@gmail.com>
2024-06-02 19:41:52 -0700

    Update tagline

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,7 @@ export default function About(): JSX.Element {
   return (
     <>
       <h2>Hey, I'm Aaron!</h2>
-      <h4>Full Stack Dev, Technical Writer, Frog Enthusiast</h4>
+      <h4>Full Stack Dev, Technical Writer</h4>
       <div id="aboutText">
         <p>
           I have always been fascinated by technology and utilizing it to help meet real needs. Programming was a favorite hobby of mine from a young age and something that I always hoped to do as a career. After 5 years in the IT industry as a team lead, documentation manager, and training supervisor, I enrolled at Epicodus to pursue my goal of being a developer.

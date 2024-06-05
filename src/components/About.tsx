608e9f42e44fe8649e055be3de749a00c59035ea
mejia-dev <aaron.mejia8484@gmail.com>
2024-06-05 00:12:39 -0700

    Update wording

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,7 +8,7 @@ export default function About(): JSX.Element {
           I have always been fascinated by technology and utilizing it to help meet real needs. Programming was a favorite hobby of mine from a young age and something that I always hoped to do as a career. After 5 years in the IT industry as a team lead, documentation manager, and training supervisor, I enrolled at Epicodus to pursue my goal of being a developer.
         </p>
         <p>
-          I'm currently interning with a company that builds case management software for nonprofits, utilizing my knowledge of C# to revamp an automated testing suite for the application in xUnit and Selenium, and making use of my technical writing expertise to update the end-user guides.
+          I'm currently working at a company that builds case management software for adoption agencies, utilizing my knowledge of C# to revamp an automated testing suite for the application in xUnit and Selenium, and making use of my technical writing expertise to update the end-user guides.
         </p>
         <p>
           In my downtime, you can find me making music, modding games, and taking photos.

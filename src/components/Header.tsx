74109893412975218f7aacfe588062be5adfb1f4
mejia-dev <aaron.mejia8484@gmail.com>
2024-06-05 02:09:01 -0700

    Remove React Router usage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import ReactModal, { Styles } from "react-modal";
-import { Link } from "react-router-dom";
 import imgProfilePic from '../assets/aaron.png';
 import imgWordArt from '../assets/name.png';
 
 interface HeaderProps {
+  onClickNavButton: Dispatch<SetStateAction<string>>;
   navButtons: string[]
 }
 
@@ -31,9 +31,9 @@ export default function Header(props: HeaderProps): JSX.Element {
         {
           props.navButtons.map((action) => (
             <span key={action + (modalShown ? "small" : "big")} className={action + (modalShown ? "small" : "big")} onClick={() => setModalShown(false)}>
-              <Link to={"/" + action.toLowerCase()}>
+              <a href="#" onClick={() => props.onClickNavButton(action)}>
                 <div className={modalShown ? "navBarButtonSmall" : "navBarButton"}>{action}</div>
-              </Link>
+              </a>
               <br />
             </span>
           ))
@@ -54,13 +54,13 @@ export default function Header(props: HeaderProps): JSX.Element {
           </div>
         </a>
 
-        <Link to="/contact">
+        <a href="#" onClick={() => props.onClickNavButton("Contact")}>
           <div className="navBarExternalLinksButton" onClick={() => setModalShown(false)}>
             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-envelope-fill" viewBox="0 0 16 16">
               <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z" />
             </svg>
           </div>
-        </Link>
+        </a>
 
       </div>
     )

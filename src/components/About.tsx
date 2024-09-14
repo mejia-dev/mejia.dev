a5cd26c05429c25617b5dc63370c0abc1c9f7a9d
mejia-dev <aaron.mejia8484@gmail.com>
2024-09-14 11:35:23 -0700

    Change wording and layout in About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,18 +1,52 @@
+import { useState } from "react";
+
 export default function About(): JSX.Element {
+
+  const [aboutOption, setAboutOption] = useState<string>("Dev");
+  function getAboutOption(): string {
+    switch (aboutOption) {
+      case "Writer": return "I provide technical writing services for items such as IT procedures, software manuals, and programming curriculum. I bridge the gap between complex technical information and readability, helping both technical experts and end users understand the material.";
+
+      case "Trainer": return "Prior to working as a developer, I worked in the IT industry for 5 years, creating training plans and material, organizing training sessions for tier 1 and tier 2 technicians, and running an internship program for those interested in starting in the industry.";
+
+      default: return "I'm a full-stack developer specializing in C#/.NET, TypeScript, and Python. I currently work as an automation specialist at a cybersecurity firm, creating software and scripts to streamline efficiency, reduce human error, and proactively analyze cyber threats.";
+    }
+  }
+
+  const menuOptions = [
+    {
+      value: "Dev",
+      text: "Full-Stack Dev",
+    },
+    {
+      value: "Writer",
+      text: "Technical Writer",
+    },
+    {
+      value: "Trainer",
+      text: "IT Trainer",
+    },
+  ]
+
   return (
-    <div>
-      <h2>Hey, I'm Aaron!</h2>
-      <h4>Full-Stack Dev, Technical Writer, IT Trainer</h4>
+    <div id="aboutContent">
+      <h1>Hey, I'm Aaron!</h1>
+      <h3>
+        {menuOptions.map((item, index) => (
+          <span key={"aboutList" + index}>
+            <a
+              href='#'
+              onClick={() => setAboutOption(item.value)}
+              className={aboutOption == item.value ? "aboutOptionSelected" : "aboutOption"}
+            >
+              {item.text}
+            </a>
+            {index < menuOptions.length - 1 ? <span> · </span> : <></>}
+          </span>
+        ))}
+      </h3>
       <div id="aboutText">
-        <p>
-          I have always been fascinated by technology and utilizing it to help meet real needs. Programming was a favorite hobby of mine from a young age and something that I always hoped to do as a career. After 5 years in the IT industry as a team lead, documentation manager, and training supervisor, I enrolled at Epicodus to pursue my goal of being a developer.
-        </p>
-        <p>
-          I'm currently working at a company that builds case management software for adoption agencies, utilizing my knowledge of C# to revamp an automated testing suite for the application in xUnit and Selenium, and making use of my technical writing expertise to update the end-user guides for the software.
-        </p>
-        <p>
-          In my downtime, you can find me recording music, modding games, and experimenting with new technologies.
-        </p>
+        {getAboutOption()}
       </div>
     </div>
   )
